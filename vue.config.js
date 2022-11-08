8458f846f3978880a5df443547465f03981651c1
101ta28 <workta28@gmail.com>
2022-11-09 01:48:02 +0900

    create docs folder

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-  publicPath: "ff14-dice-sort",
+  outputDir: "./docs",
   transpileDependencies: ["vuetify"],
 });
