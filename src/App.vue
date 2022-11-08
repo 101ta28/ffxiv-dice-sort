993023f65ca909176a019100c3659f0da5c840d9
101ta28 <workta28@gmail.com>
2022-11-09 01:43:49 +0900

    vue installed

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-app>
+    <v-app-bar app color="primary" dark>
+      <div class="d-flex align-center">
+        <h2>FF14 Dice Sort</h2>
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+        text
+      >
+        <span class="mr-2">Latest Release</span>
+        <v-icon>mdi-open-in-new</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-main>
+      <InputArea />
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import InputArea from "./components/InputArea";
+
+export default {
+  name: "App",
+
+  components: {
+    InputArea,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ src/components/InputArea.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h2>{{ title }}</h2>
+    <p>{{ text }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: "Body",
+      text: "Have a good day!",
+    };
+  },
+};
+</script>
